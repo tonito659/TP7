ee278a0266c9354b0da1f5fe4b90ba0b7b244fbc
Antoine <fallenhaswon@gmail.com>
2016-11-25 12:05:18 +0100

    initial commit

---
+++ src/com/antoine/Etudiant.java
@@ -0,0 +1,12 @@
+package com.antoine;
+
+/**
+ * Created by Antoine on 25/11/2016.
+ */
+public class Etudiant
+{
+    int numeroo;
+    String prenom;
+    String nom;
+
+}
--- src/com/antoine/ListeEtudiant.java
+++ src/com/antoine/ListeEtudiant.java
@@ -1,6 +1,7 @@
 package com.antoine;
 
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Antoine on 25/11/2016.
@@ -19,19 +20,56 @@ public class ListeEtudiant {
 
     }
 
-    public void ajouteEtudiantDebut(ListeEtudiant, int numero, String prenom, String nom){
+    ////////////////////////////////////////////////////
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNom() {
+
+        return nom;
+    }
+    //////////////////////////////////////////////////////
+
+
+
+
+    public void ajouteEtudiantDebut(ListeEtudiant liste, int numero, String prenom, String nom){
 
     }
 
-    public void ajouteEtudiantFin(ListeEtudiant, int numero, String prenom, String nom){
+    public void ajouteEtudiantFin(ListeEtudiant liste, int numero, String prenom, String nom){
 
     }
 
-    public ListeEtudiant (ListeEtudiant liste, int numero){
+    public ListeEtudiant getEtudiant (ListeEtudiant liste, int numero){
+        // permet d'obtenir la liste d
 
     }
 
     public void afficheListe (ListeEtudiant maliste){
+
+    }
+
+
+
+    public void trieListeParNumero (ListeEtudiant maliste){
 
     }
 

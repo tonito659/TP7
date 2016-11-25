46597e751e458378a5fc346e3cab5054ebef1fe6
Antoine <fallenhaswon@gmail.com>
2016-11-25 11:50:20 +0100

    Initial commit

---
+++ src/com/antoine/ListeEtudiant.java
@@ -0,0 +1,38 @@
+package com.antoine;
+
+import java.util.List;
+
+/**
+ * Created by Antoine on 25/11/2016.
+ */
+public class ListeEtudiant {
+
+    int numero;
+    String prenom;
+    String nom;
+    ListeEtudiant suivant;
+
+    public ListeEtudiant(int numero, String prenom, String nom){
+        this.numero = numero;
+        this.prenom = prenom;
+        this.nom = nom;
+
+    }
+
+    public void ajouteEtudiantDebut(ListeEtudiant, int numero, String prenom, String nom){
+
+    }
+
+    public void ajouteEtudiantFin(ListeEtudiant, int numero, String prenom, String nom){
+
+    }
+
+    public ListeEtudiant (ListeEtudiant liste, int numero){
+
+    }
+
+    public void afficheListe (ListeEtudiant maliste){
+
+    }
+
+}
